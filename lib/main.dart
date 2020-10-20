91fb39346dff6746e96c5651c18b07fcce263001
Hitalo <enghitalo@gmail.com>
2020-10-19 22:07:15 -0300

    Avanços gerais

--- lib/main.dart
+++ lib/main.dart
@@ -5,26 +5,13 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,17 +19,13 @@ class MyApp extends StatelessWidget {
   }
 }
 
+double paperize = 842;
+var a4 = true;
+String folha_nome = 'A4';
+double fator_preco = 1;
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,85 +38,182 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
+  void fn_aFour() {
+    // a4 = !a4;
+    print(a4);
+
+    if (a4) {
+      setState(() {
+        paperize = 842;
+        folha_nome = 'A4';
+        fator_preco = 1;
+      });
+      a4 = !a4;
+    } else {
+      setState(() {
+        paperize = 842 / 2;
+        folha_nome = 'A5';
+
+        fator_preco = 0.8;
+      });
+      a4 = !a4;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       backgroundColor: Colors.grey,
-      // appBar: AppBar(
-      //   // Here we take the value from the MyHomePage object that was created by
-      //   // the App.build method, and use it to set our appbar title.
-      //   title: Text(widget.title),
-      // ),
       body: Center(
         child: Container(
-          padding: EdgeInsets.all(20),
-       
-          color: Colors.white,
-
-          height: 842,
-          width: 595,
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [Center(child: Text('Cerveja'))],
-              ),
-              Row(
-                children: [Text('Brahma')],
-              ),
-              Row(
-                children: [Text('Puro Malte')],
-              ),
-              Row(
-                children: [Text('12x696')],
-              ),
-              Row(
-                children: [Text('R\$')],
-              ),
-              Row(
-                children: [Text('28,45')],
-              ),
-            ],
+          decoration: BoxDecoration(
+            color: Colors.white,  
+            border: Border.all(width: .5),
           ),
+          padding: EdgeInsets.all(20 * fator_preco),
+          height: paperize,
+          width: 595,
+          child:
+              // // Column(
+              // //   mainAxisAlignment: MainAxisAlignment.start,
+              // //   children: <Widget>[
+
+              Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(width: 2),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+            child: Stack(
+              children: [
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                            width: 500,
+                            child: Text(
+                              'PURIFICADOR DE AR',
+                              textAlign: TextAlign.center,
+                              softWrap: true,
+                              style: TextStyle(
+                                fontSize: 30 * fator_preco,
+                                fontFamily: 'Qanelas',
+                              ),
+                            )),
+                      ],
+                    ),
+                    Center(
+                      child: SizedBox(
+                        child: Column(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(top: 10 * fator_preco),
+                              width: 553,
+                              child: Text(
+                                'Marmeladinha caseira'.toUpperCase(),
+                                textAlign: TextAlign.center,
+                                softWrap: true,
+                                style: TextStyle(
+                                  fontSize: 63 * fator_preco,
+                                  fontFamily: 'Qanelas',
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(top: 10 * fator_preco),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'Puro Malte'.toUpperCase(),
+                                    textAlign: TextAlign.center,
+                                    softWrap: true,
+                                    style: TextStyle(
+                                      fontSize: 25 * fator_preco,
+                                    ),
+                                  ),
+                                  Text(
+                                    '12x696ml',
+                                    textAlign: TextAlign.center,
+                                    softWrap: true,
+                                    style: TextStyle(
+                                      fontSize: 25 * fator_preco,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Positioned(
+                  bottom: -35 * (1 / fator_preco),
+                  left: 0,
+                  right: 0,
+                  child: Column(
+                    children: [
+                      Divider(
+                        indent: 40,
+                        endIndent: 20,
+                        height: 0,
+                        thickness: 2,
+                        color: Colors.black,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'R\$ ',
+                            softWrap: true,
+                            style: TextStyle(
+                              fontSize: 50 * fator_preco,
+                            ),
+                          ),
+                          Text(
+                            '28',
+                            softWrap: true,
+                            style: TextStyle(
+                                fontSize: 250 * fator_preco,
+                                color: Colors.black),
+                          ),
+                          Text(
+                            ',45',
+                            softWrap: true,
+                            style: TextStyle(
+                              fontSize: 100 * fator_preco,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+
+          // ],
         ),
       ),
+      // ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          _incrementCounter();
+          fn_aFour();
+        },
         tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Text(folha_nome),
+      ),
     );
   }
 }
