0806f2ac8b4aee8605ab3f840d72ada46168bedb
Hitalo <enghitalo@gmail.com>
2020-10-21 15:52:05 -0300

    Adição de buttons e drop-down de nevageção

--- README.md
+++ README.md
@@ -2,15 +2,5 @@
 
 Esse projeto tem como objetivo criar placas de promoções, placas em tamanho A4, A5 e 595x210 em vários design diferentes extraindo os dados de de uma planilha e emprimindo em batch
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+##Tela inicial
+![til](assets/apresentacao/placas_tela_inicial.png)
--- lib/main.dart
+++ lib/main.dart
@@ -68,142 +68,203 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey,
-      body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(width: .5),
-          ),
-          padding: EdgeInsets.all(20 * fator_preco),
-          height: paperize,
-          width: 595,
-          child:
-              // // Column(
-              // //   mainAxisAlignment: MainAxisAlignment.start,
-              // //   children: <Widget>[
-
+      body: Stack(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
               Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(width: 2),
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-            ),
-            child: Stack(
-              children: [
-                Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                            width: 500,
-                            child: Text(
-                              'PURIFICADOR DE AR',
-                              textAlign: TextAlign.center,
-                              softWrap: true,
-                              style: TextStyle(
-                                fontSize: 30 * fator_preco,
-                                fontFamily: 'Qanelas',
-                              ),
-                            )),
-                      ],
-                    ),
-                    Center(
-                      child: SizedBox(
-                        child: Column(
-                          children: [
-                            Container(
-                              margin: EdgeInsets.only(top: 10 * fator_preco),
-                              width: 553,
-                              child: Text(
-                                'Marmeladinha caseira'.toUpperCase(),
-                                textAlign: TextAlign.center,
-                                softWrap: true,
-                                style: TextStyle(
-                                  fontSize: 63 * fator_preco,
-                                  fontFamily: 'Qanelas',
-                                ),
-                              ),
-                            ),
-                            Container(
-                              margin: EdgeInsets.only(top: 10 * fator_preco),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    'Puro Malte'.toUpperCase(),
+                width: 40,
+                height: 40,
+                child: RaisedButton(
+                  color: Colors.white,
+                  onPressed: () {},
+                  child: Icon(
+                    Icons.arrow_left,
+                  ),
+                ),
+                margin: EdgeInsets.all(10),
+              ),
+              Listener(
+                onPointerDown: (event) {
+                  print(event.position-event.localPosition);
+
+                },
+                child: Container(
+                  padding: EdgeInsets.only(left: 10, right: 10),
+                  height: 51,
+                  decoration: BoxDecoration(color: Colors.white),
+                  child: Column(
+                    children: [
+                      Text(
+                        'PROMOÇÃO',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Icon(Icons.arrow_drop_down)
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                width: 40,
+                height: 40,
+                child: RaisedButton(
+                  color: Colors.white,
+                  onPressed: () {},
+                  child: Icon(
+                    Icons.arrow_right,
+                  ),
+                ),
+                margin: EdgeInsets.all(10),
+              ),
+            ],
+          ),
+          Center(
+              child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(width: .5),
+                ),
+                padding: EdgeInsets.all(20 * fator_preco),
+                height: paperize,
+                width: 595,
+                child:
+                    // // Column(
+                    // //   mainAxisAlignment: MainAxisAlignment.start,
+                    // //   children: <Widget>[
+
+                    Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(width: 2),
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  child: Stack(
+                    children: [
+                      Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                  width: 500,
+                                  child: Text(
+                                    'PURIFICADOR DE AR',
                                     textAlign: TextAlign.center,
                                     softWrap: true,
                                     style: TextStyle(
-                                      fontSize: 25 * fator_preco,
+                                      fontSize: 30 * fator_preco,
+                                      fontFamily: 'Qanelas',
+                                    ),
+                                  )),
+                            ],
+                          ),
+                          Center(
+                            child: SizedBox(
+                              child: Column(
+                                children: [
+                                  Container(
+                                    margin:
+                                        EdgeInsets.only(top: 10 * fator_preco),
+                                    width: 553,
+                                    child: Text(
+                                      'Marmeladinha caseira'.toUpperCase(),
+                                      textAlign: TextAlign.center,
+                                      softWrap: true,
+                                      style: TextStyle(
+                                        fontSize: 63 * fator_preco,
+                                        fontFamily: 'Qanelas',
+                                      ),
                                     ),
                                   ),
-                                  Text(
-                                    '12x696ml',
-                                    textAlign: TextAlign.center,
-                                    softWrap: true,
-                                    style: TextStyle(
-                                      fontSize: 25 * fator_preco,
+                                  Container(
+                                    margin:
+                                        EdgeInsets.only(top: 10 * fator_preco),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          'Puro Malte'.toUpperCase(),
+                                          textAlign: TextAlign.center,
+                                          softWrap: true,
+                                          style: TextStyle(
+                                            fontSize: 25 * fator_preco,
+                                          ),
+                                        ),
+                                        Text(
+                                          '12x696ml',
+                                          textAlign: TextAlign.center,
+                                          softWrap: true,
+                                          style: TextStyle(
+                                            fontSize: 25 * fator_preco,
+                                          ),
+                                        )
+                                      ],
                                     ),
                                   )
                                 ],
                               ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Positioned(
-                  bottom: -35 * (1 / fator_preco),
-                  left: 0,
-                  right: 0,
-                  child: Column(
-                    children: [
-                      Divider(
-                        indent: 40,
-                        endIndent: 20,
-                        height: 0,
-                        thickness: 2,
-                        color: Colors.black,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'R\$ ',
-                            softWrap: true,
-                            style: TextStyle(
-                              fontSize: 50 * fator_preco,
-                            ),
-                          ),
-                          Text(
-                            '27',
-                            softWrap: true,
-                            style: TextStyle(
-                                fontSize: 250 * fator_preco,
-                                color: Colors.black),
-                          ),
-                          Text(
-                            ',45',
-                            softWrap: true,
-                            style: TextStyle(
-                              fontSize: 100 * fator_preco,
                             ),
                           ),
                         ],
                       ),
+                      Positioned(
+                        bottom: -35 * (1 / fator_preco),
+                        left: 0,
+                        right: 0,
+                        child: Column(
+                          children: [
+                            Divider(
+                              indent: 40,
+                              endIndent: 20,
+                              height: 0,
+                              thickness: 2,
+                              color: Colors.black,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'R\$ ',
+                                  softWrap: true,
+                                  style: TextStyle(
+                                    fontSize: 50 * fator_preco,
+                                  ),
+                                ),
+                                Text(
+                                  '27',
+                                  softWrap: true,
+                                  style: TextStyle(
+                                      fontSize: 250 * fator_preco,
+                                      color: Colors.black),
+                                ),
+                                Text(
+                                  ',45',
+                                  softWrap: true,
+                                  style: TextStyle(
+                                    fontSize: 100 * fator_preco,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
                     ],
                   ),
-                )
-              ],
-            ),
-          ),
+                ),
 
-          // ],
-        ),
+                // ],
+              ),
+              Image(
+                image: AssetImage('assets/images/Oferta-G.png'),
+              )
+            ],
+          )),
+        ],
       ),
       // ),
       floatingActionButton: FloatingActionButton(
